72c05fa2ab3ea0937f5c71a38aac07d9d6f77ece
xuhongfei <xuhongfei@163.com>
2018-08-03 21:53:21 +0800

    1

--- ChineseCulture/ChineseCulture.Model/HomePageViewModel.cs
+++ ChineseCulture/ChineseCulture.Model/HomePageViewModel.cs
@@ -19,6 +19,16 @@ namespace ChineseCulture.Model
        
         public List<HomeCategoryArticleViewModel> XuexiaoFirst { get; set; }
         public List<HomeCategoryArticleViewModel> XuexiaoDetail { get; set; }
+        public List<HomeCategoryArticleViewModel> PeixunjigouFirst { get; set; }
+        public List<HomeCategoryArticleViewModel> PeixunjigouDetail { get; set; }
+        public List<HomeCategoryArticleViewModel> QiuzhizhaopinFirst { get; set; }
+        public List<HomeCategoryArticleViewModel> QiuzhizhaopinDetail { get; set; }
+        public List<HomeCategoryArticleViewModel> ZhuanjiajiangshiFirst { get; set; }
+        public List<HomeCategoryArticleViewModel> ZhuanjiajiangshiDetail { get; set; }
+        public List<HomeCategoryArticleViewModel> XiaoyuanmingrenFirst { get; set; }
+        public List<HomeCategoryArticleViewModel> XiaoyuanmingrenDetail { get; set; }
+        public List<HomeCategoryArticleViewModel> ChuangyehezuoFirst { get; set; }
+        public List<HomeCategoryArticleViewModel> ChuangyehezuoDetail { get; set; }
     }
 
 }
