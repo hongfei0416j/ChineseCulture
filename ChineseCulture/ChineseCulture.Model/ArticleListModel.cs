f85b79ba91f887dc5b3f087eeb0c67aaa995d5f0
xuhongfei <xuhongfei@163.com>
2018-08-14 18:09:41 +0800

    111

---
+++ ChineseCulture/ChineseCulture.Model/ArticleListModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChineseCulture.Model
+{
+    public class ArticleListModel
+    {
+        public string dateStart { set; get; }
+        public string dateEnd { set; get; }
+        public int category_id { set; get; }
+        public string article_title { set; get; }
+        public List<Article> articleList { set; get; }
+
+        public int allCount { set; get; }
+
+
+    }
+}
